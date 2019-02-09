4db8919154064746f2619c7e35b3323ca35379e8
Yuri <yuri.tayama@linksports.co.jp>
2019-02-09 18:25:12 +0900

    📦add & link react-native-voice

--- android/app/src/main/java/com/pre_sakeboard/MainApplication.java
+++ android/app/src/main/java/com/pre_sakeboard/MainApplication.java
@@ -3,6 +3,7 @@ package com.pre_sakeboard;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wenkesj.voice.VoicePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new VoicePackage(),
           new VectorIconsPackage(),
           new RNFirebasePackage()
       );
