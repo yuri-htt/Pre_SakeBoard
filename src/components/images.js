a4978c001697c863ecdb69297de328fc68d92d53
Yuri <yuri.tayama@linksports.co.jp>
2019-02-09 18:33:20 +0900

    👍add images

---
+++ src/components/images.js
@@ -0,0 +1,43 @@
+const cooktail = require('../../assets/images/cooktail.png');
+const wine = require('../../assets/images/wine.png');
+const beer = require('../../assets/images/beer.png');
+const sake = require('../../assets/images/sake.png');
+const syotyu = require('../../assets/images/syotyu.png');
+const whisky = require('../../assets/images/whisky.png');
+const cooktail_w = require('../../assets/images/cooktail_w.png');
+const wine_w = require('../../assets/images/wine_w.png');
+const beer_w = require('../../assets/images/beer_w.png');
+const sake_w = require('../../assets/images/sake_w.png');
+const syotyu_w = require('../../assets/images/syotyu_w.png');
+const whisky_w = require('../../assets/images/whisky_w.png');
+const button = require('../../assets/images/button.png');
+const logo = require('../../assets/images/logo.png');
+const logoTxt = require('../../assets/images/logo_txt.png');
+const mike = require('../../assets/images/mike.png');
+const dotsCircle = require('../../assets/images/dotsCircle.png');
+const voiceShape = require('../../assets/images/voiceShape.png');
+const logoHeart = require('../../assets/images/logo_heart.png');
+
+const images = {
+  cooktail,
+  wine,
+  beer,
+  sake,
+  syotyu,
+  whisky,
+  cooktail_w,
+  wine_w,
+  beer_w,
+  sake_w,
+  syotyu_w,
+  whisky_w,
+  button,
+  logo,
+  logoTxt,
+  mike,
+  dotsCircle,
+  voiceShape,
+  logoHeart,
+};
+
+export default images;
